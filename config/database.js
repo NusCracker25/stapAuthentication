a6ee14bc2989b2a6f4df5b3a35f063e9c77418c5
NutsCracker25 <nutscracker25@gmail.com>
2020-05-27 06:57:27 +0200

    add new dependencies for proper logging on server : morgan, file rotation on winston and clf date format

--- config/database.js
+++ config/database.js
@@ -1,7 +1,11 @@
 module.exports={
-    database:'mongodb://localhost:27017/meanauth',
-    userscollection: 'users',
-    logcollection: 'authlog',
-
+    database:{
+        url:'mongodb://localhost:27017/meanauth',
+        userscollection: 'users',
+        logcollection: 'authlog'
+    },
+    logging:{
+        folder : 'logs'
+    },
     secret:'asecret'
 }
